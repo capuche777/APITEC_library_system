cd9f8e52e247e158c0244df72b6a0eff9cec6cba
Jeremias Enriquez <capuche777@gmail.com>
2018-03-22 11:49:14 -0600

    register form has been structured

---
+++ README.md
@@ -0,0 +1,7 @@
+# APITEC
+##### APITEC Curso: Desarrollo Web
+##### Catedrático: Carlos Rafael Loarca
+##### Ejercicio: Sistema de Biblioteca
+----------
+###### FASE 1: Sistema de control de libros
+Se requiere la realización de un sistema de biblioteca que permita el manejo de libros, autores, temas, para que luego en la segunda fase se realice el sistema de prestamos. El sistema debe contar con login y registro del administrador del sistema, y una vez logueado, podrá agregar, modificar y eliminar libros, autores y temas
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var stripCssComments = require('gulp-strip-css-comments');
+var concat = require('gulp-concat');
+
+gulp.task('pug', function() {
+    gulp.src('./dev/pug/*.pug')
+    .pipe(pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('css', function() {
+    return gulp.src('./dev/sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/assets/css'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('images', function () {
+    return gulp.src('dev/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/img'))
+});
+
+gulp.task('copy', function () {
+    return gulp.src('dev/scripts/*.js')
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(browserSync.stream())
+})
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('./dev/scripts/*.js')
+      .pipe(concat('script.js'))
+      .pipe(stripCssComments(false))
+      .pipe(gulp.dest('./dist/assets/js'))
+      .pipe(browserSync.stream())
+  });
+
+gulp.task('watch', ['pug', 'scripts', 'browserSync', 'css'], function () {
+    gulp.watch('dev/pug/**/*.pug', ['pug']);
+    gulp.watch('dev/sass/**/*.scss', ['css']);
+    gulp.watch('dev/scripts/**/*.js', ['scripts']);
+});
