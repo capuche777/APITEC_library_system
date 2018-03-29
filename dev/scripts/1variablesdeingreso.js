4a2dafcbba78301408c548e911d3ecee45f49b57
Jeremias Enriquez <capuche777@gmail.com>
2018-03-28 22:14:46 -0600

    Los departamentos son insertados dinamicamente en el documento

--- dev/scripts/1variablesdeingreso.js
+++ dev/scripts/1variablesdeingreso.js
@@ -6,3 +6,9 @@ var libros = [];
 
 // Array que almacena los autores ingresados
 var autores = [];
+
+// Array que almacena los departamentos de Guatemala
+let Departamento = [];
+
+// Contenedor de la lista de departamentos
+const guate = ["Alta Verapaz", "Baja Verapaz", "Chimaltenango", "Chiquimula", "Petén", "El Progreso", "Quiché", "Escuintla", "Guatemala", "Huehuetenango", "Izabal", "Jalapa", "Jutiapa", "Quetzaltenango", "Retalhuleu", "Sacatepéquez", "San Marcos", "Santa Rosa", "Sololá", "Suchitepéquez", "Totonicapán", "Zacapa"];
--- dev/scripts/2departamentos.js
+++ dev/scripts/2departamentos.js
@@ -1,15 +1,26 @@
-// Array que almacena los departamentos de Guatemala
-let Departamento = [];
-
-const guate = ["Alta Verapaz", "Baja Verapaz", "Chimaltenango", "Chiquimula", "Petén", "El Progreso", "Quiché", "Escuintla", "Guatemala", "Huehuetenango", "Izabal", "Jalapa", "Jutiapa", "Quetzaltenango", "Retalhuleu", "Sacatepéquez", "San Marcos", "Santa Rosa", "Sololá", "Suchitepéquez", "Totonicapán", "Zacapa"];
-
 // Funcion encargada de crear dinamicamente el objeto Departamento basado en la variable guate
+
+// Funcion para crear los departamentos
 function crearDeptos(el, i, arr) {
     let _depto = {
         departamento_id: i+1,
         nombre: el
     }
     Departamento.push(_depto)
+    
+    // Selecciona el Select para insertar los departamentos
+    var select = document.querySelector('#depto');
+    // Crea la etiqueta Option vacia
+    var optionDepto = document.createElement('option');
+    // Indica el texto a insertar en el select
+    var optionContent = document.createTextNode(`${el}`)
+
+    // Inserta el atributo value a cada option
+    optionDepto.setAttribute('value', `${i+1}`)
+    // Le inserta el texto a cada uno de los options
+    optionDepto.appendChild(optionContent);
+    // Inserta cada option en el Select
+    select.appendChild(optionDepto);
 }
 
 guate.forEach(crearDeptos);
